0b1b89dd0d5be2f33b35370899159b2ab96b5537
helado de brownie <helado.de.brownie@gmail.com>
2019-12-13 01:04:54 -0500

    setup html page

---
+++ main.js
@@ -0,0 +1,48 @@
+void function () {
+    'use strict'
+
+    var $ = document.getElementById.bind(document)
+
+    function show(result, isResultOfError) {
+        if (isResultOfError === undefined) {
+            isResultOfError = false
+        }
+
+        if (isResultOfError) {
+            $('result').textContent = result
+        } else {
+            location.hash = encodeURIComponent(result)
+        }
+    }
+
+    function withoutHash(string) {
+        return string.replace(/^#/, '')
+    }
+
+    addEventListener('error', show.bind(null, 'Oops, something broke.', true))
+    $('back').addEventListener('click', history.back.bind(history))
+    $('forward').addEventListener('click', history.forward.bind(history))
+
+    function handleHashChange() {
+        var result = decodeURIComponent(withoutHash(location.hash))
+        $('result').textContent = result
+    }
+
+    addEventListener('hashchange', handleHashChange)
+
+    var rerollElement = $('reroll')
+
+    rerollElement.addEventListener('click', function () {
+        show(grammar.flatten('#origin#'))
+    })
+
+    if (withoutHash(location.hash) === '') {
+        rerollElement.click()
+    } else {
+        handleHashChange()
+    }
+
+    if (typeof Itch !== 'undefined' && Itch instanceof Object) {
+        $('back').parentElement.hidden = false
+    }
+}()
